38288c50580c602e6b32afaf41bb23b300adfd48
KalapisEdy1 <85459959+KalapisEdy1@users.noreply.github.com>
2026-02-19 00:27:41 +0200

    Add files via upload

---
+++ Services/EmailSender.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+public class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        // Datele de la Mailtrap (le iei din panoul lor)
+        var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+        {
+            Credentials = new NetworkCredential("75214fb34ada4c", "d554b99e7aac52"),
+            EnableSsl = true
+        };
+
+        var mailMessage = new MailMessage("kalapisedy2@gmail.com", email, subject, htmlMessage)
+        {
+            IsBodyHtml = true
+        };
+
+        return client.SendMailAsync(mailMessage);
+    }
+}
